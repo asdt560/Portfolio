ef76b3c01437aa5af2f6f0fecf92df839be496f3
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-06 18:45:06 -0300

    Add dynamic cards

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ function menuClose() {
 
 const arr = [
   {
+    id: '1',
     name: 'Multi-Post Stories',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    featuredimage: './images/Img-Placeholder(1).png',
+    extradescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featuredimage: './images/Snapshoot-Popup.svg',
     technologies: [
       'html', 'css', 'javascript',
     ],
@@ -20,9 +22,11 @@ const arr = [
     source: 'https://github.com/asdt560/Portfolio',
   },
   {
+    id: '2',
     name: 'Multi-Post Stories',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    featuredimage: './images/Img-Placeholder(1).png',
+    extradescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featuredimage: './images/Snapshoot-Popup.svg',
     technologies: [
       'html', 'css', 'javascript',
     ],
@@ -30,9 +34,11 @@ const arr = [
     source: 'https://github.com/asdt560/Portfolio',
   },
   {
+    id: '3',
     name: 'Multi-Post Stories',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    featuredimage: './images/Img-Placeholder(1).png',
+    extradescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featuredimage: './images/Snapshoot-Popup.svg',
     technologies: [
       'html', 'css', 'javascript',
     ],
@@ -40,9 +46,11 @@ const arr = [
     source: 'https://github.com/asdt560/Portfolio',
   },
   {
+    id: '4',
     name: 'Multi-Post Stories',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    featuredimage: './images/Img-Placeholder(1).png',
+    extradescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featuredimage: './images/Snapshoot-Popup.svg',
     technologies: [
       'html', 'css', 'javascript',
     ],
@@ -50,21 +58,62 @@ const arr = [
     source: 'https://github.com/asdt560/Portfolio',
   },
 ];
+
+const ret = (arr) => {
+  let tech = '';
+  arr.forEach((item) => {
+    tech += `<li>${item}</li>`;
+    return tech;
+  });
+  return tech;
+};
 const cards = document.getElementById('cards');
-console.log(Array.of(cards))
-arr.forEach((item, index) => {
+arr.forEach((item) => {
   cards.innerHTML += `<section id="c1" class="card">
   <img id="project1image" class="placeholder-image" src=${item.featuredimage} alt="A Placeholder">
   <div class="card-text">
     <h5 class="card-title" id="project1title">${item.name}</h5>
     <p class="blurb2" id="project1blurb">${item.description}</p>
     <ul class="card-list" id="project1list">
+    ${ret(item.technologies, item)}
     </ul>
-    <button class="project-button">See Project</button>
+    <button id=${item.id} class="project-button openerbutton">See Project</button>
   </div>
 </section>`;
-  /* let card = cards; */
 });
+const popup = document.getElementById('popup');
+
+const popupbutton = document.getElementsByClassName('openerbutton');
+
+function popupOpen(id) {
+  arr.forEach((item) => {
+    if (id === item.id) {
+      popup.innerHtTML += `<div id="popupcontainer">
+    <div id="X-popup">
+      <h5 class="popuptitle">${item.name}</h5>
+      <a class="menu-close">
+        <img class="image-X" alt="Close" src="./images/Icon.png" onclick="popupClose()">
+      </a>
+    </div>
+    <img class="popupimage" src=${item.featuredimage} alt="placeholder image">
+    <p class="popuptext">${item.extradescription}</p>
+    <ul class="card-list">
+    ${ret(item.technologies, item)}
+    </ul>
+    <button class="project-button popupbutton">See Live<img src="./images/Live.svg" alt="Live"></button>
+    <button class="project-button popupbutton">See Source <img src="./images/Normal-Button/Tertiary/Icons/Vector.png" alt="Github"></button>
+  </div>`;
+    }
+  });
+}
+
+popupbutton.forEach((button) => {
+  button.addEventListener('click', popupOpen(button.id));
+});
+
+const test = document.getElementById('main-title');
+
+test.addEventListener('click', popupOpen('1'));
 /* const project1 = arr[0];
 console.log(project1);
 window.addEventListener('load', () => {
